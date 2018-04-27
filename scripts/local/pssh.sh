407b894e356ac1a6851435c0c104e3d4bd0d8108
hanxinlei <khaosminded@vip.qq.com>
2018-04-26 23:19:33 -0700

    pssh.sh

--- scripts/local/pssh.sh
+++ scripts/local/pssh.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-command='sysbench --test=cpu --cpu-max-prime=40000 --num-threads=1 run'
-pssh -i -h hostfile_pssh -x "-i ~/.ssh/as0.pem" $command
+set -f
+id=$(date -u +%s)
+_task='20 * * * * ubuntu python3  ~/SCRIPT/remote/run.py -c 10 -i '$id
+task=\'$_task\'
+psshcommand='set -f && cp /etc/crontab . && echo '$task' >> crontab && sudo mv crontab /etc/crontab'
+pssh -i -h hostfile_pssh -x "-i ~/.ssh/as0.pem" $psshcommand
