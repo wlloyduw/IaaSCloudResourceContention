4899a6c6de206808121ede9e9f9a05f243e1dfd4
Wes Lloyd <wlloyd@uw.edu>
2019-07-25 20:25:42 -0700

    pg move to ephemeral drive- dont reconfig make symb link instead

--- scripts/remote/init_pg_on_localdiskc3.bash
+++ scripts/remote/init_pg_on_localdiskc3.bash
@@ -7,13 +7,14 @@ then
   sudo service postgresql stop
   sleep 30
   #Move the database files to the new data disk:
-  sudo mv /var/lib/postgresql/9.5/main /mnt/postgres-data9.5
+  sudo mv /var/lib/postgresql/9.5/main /mnt/main
+  sudo -u postgres ln -s /mnt/main /var/lib/postgresql/9.5/main 
   sleep 10
   #Edit postgresql.conf:
-  CFG=/etc/postgresql/9.5/main/postgresql.conf
-  K=data_directory
-  V=\'/mnt/postgres-data9.5\'
-  sudo sed -i "/^$K/c$K = $V" $CFG
+  #CFG=/etc/postgresql/9.5/main/postgresql.conf
+  #K=data_directory
+  #V=\'/mnt/postgres-data9.5\'
+  #sudo sed -i "/^$K/c$K = $V" $CFG
 
   #Start Postgres:
   sudo service postgresql start
