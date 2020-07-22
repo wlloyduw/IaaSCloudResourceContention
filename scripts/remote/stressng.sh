da86899c495d5694eb415712789d272aacbc305e
SinglaAsmita <asmitasingla19@gmail.com>
2020-07-21 18:52:03 -0700

    Adding looping to stresss-ng run

--- scripts/remote/stressng.sh
+++ scripts/remote/stressng.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 date +”%T.%3N”
-stress-ng --malloc 1000 --malloc-ops 100000 --malloc-bytes 40000000 --fault 1024 --fault-ops 500000
+
+sleep=1
+
+for i in {1..6}
+do
+   stress-ng --malloc 1000 --malloc-ops 100000 --malloc-bytes 40000000 --fault 1024 --fault-ops 5000
+   sleep $sleep
+done
+
 date +”%T.%3N”
 grep -m 1 -i pgfault /proc/vmstat | cut -d' ' -f 2
 grep -m 1 -i pgmajfault /proc/vmstat | cut -d' ' -f 2
