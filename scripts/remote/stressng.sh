c005fc2191e217bcc6b7b31f0aa03699539e93c4
SinglaAsmita <asmitasingla19@gmail.com>
2020-07-23 18:52:50 -0700

    Updating Stress-ng configuration

--- scripts/remote/stressng.sh
+++ scripts/remote/stressng.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 date +%T.%3N
 
-sleep=0.5
+sleep=0.1
 
-for i in {1..5}
+for i in {1..8}
 do
-   stress-ng --malloc 1000 --malloc-ops 100000 --malloc-bytes 40000000 --fault 1024 --fault-ops 5000
+   stress-ng --malloc 1000 --malloc-ops 100000 --malloc-bytes 40000000 --fault 500 --fault-ops 500
    sleep $sleep
 done
 
